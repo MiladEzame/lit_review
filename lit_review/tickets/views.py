258f1c26294188b34f666c9e912f723c962b1f01
MiladEzame <milad.ezame@gmail.com>
2022-01-25 16:04:31 +0800

    setup ok

--- lit_review/tickets/views.py
+++ lit_review/tickets/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from tickets.models import Ticket
 
 # Create your views here.
+def index(request):
+    tickets = Ticket.objects.all()
+    return render(request, 'tickets/index.html',
+                  context={'tickets' : tickets})
+
+def home(request):
+    return render(request, 'tickets/home.html')
