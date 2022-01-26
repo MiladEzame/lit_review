3bfbf5337e6ff33e5986013120394436d30ed5e1
MiladEzame <milad.ezame@gmail.com>
2022-01-27 00:24:42 +0800

    first tickets added and index page showing all tickets ok

--- lit_review/tickets/models.py
+++ lit_review/tickets/models.py
@@ -5,3 +5,6 @@ class Ticket(models.Model):
     title = models.fields.CharField(max_length=100 , null=True)
     content = models.fields.CharField(max_length=100 , null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.title
