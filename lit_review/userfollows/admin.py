8e04ba49918d6d9c0be90a9dd694e91010435dc5
MiladEzame <milad.ezame@gmail.com>
2022-01-28 23:25:05 +0800

    search bar users + settings

--- lit_review/userfollows/admin.py
+++ lit_review/userfollows/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import UserFollows
+from .models import UserFollow
 
-admin.site.register(UserFollows)
+admin.site.register(UserFollow)
