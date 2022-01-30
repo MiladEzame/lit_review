844580bdcbed969832e01e981fc5953bdbe88449
MiladEzame <milad.ezame@gmail.com>
2022-01-30 22:00:47 +0800

    changed profile page, trying to access followers and followings

--- lit_review/userfollows/models.py
+++ lit_review/userfollows/models.py
@@ -6,8 +6,13 @@ class UserFollow(models.Model):
     user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='following')
     followed_user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='followed_by')
     time_created = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user.first_name} follows {self.followed_user.first_name}"
     
     class Meta:
         # ensures we don't get multiple UserFollows instances
         # for unique user-user_followed pairs
         unique_together = ('user', 'followed_user', )
+        
