992d3cdadd1ad6d51b405bd78f7bacec9006bdca
MiladEzame <milad.ezame@gmail.com>
2022-02-02 20:31:38 +0800

    paused

--- lit_review/userfollows/views.py
+++ lit_review/userfollows/views.py
@@ -4,10 +4,10 @@ from .models import User, UserFollow
 # Create your views here.
 def user_followers(request):
     user = User.objects.get(id=4)
-    followed_by = UserFollow.objects.filter(followed_user = user).all()
+    followers = UserFollow.objects.filter(followed_user = user).all()
     following = UserFollow.objects.filter(user = user).all()
 
     return render(
         request, 'user/profile.html', 
-        context={'user': user, 'followed_by': followed_by, 'following': following}
+        context={'user': user, 'followers': followers, 'following': following}
     )
