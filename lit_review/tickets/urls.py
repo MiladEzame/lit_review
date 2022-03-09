79edb4643dab1873fd7c0acdfc8de713779e3c93
MiladEzame <milad.ezame@gmail.com>
2022-03-09 12:32:15 +1000

    added current user to ticket creation and working on showing friends feed on the dashbord.

--- lit_review/tickets/urls.py
+++ lit_review/tickets/urls.py
@@ -6,6 +6,7 @@ urlpatterns = [
     path('home/', views.home, name='home'),
     path('tickets/', views.tickets, name='tickets'),
     path('create_ticket/', views.createTicket, name='create_ticket'),
+    path('dashboard/<int:ticket_id>', views.createReview, name='create_review'),
     path('update_ticket/<str:pk>/', views.updateTicket, name='update_ticket'),
     path('delete_ticket/<str:pk>/', views.deleteTicket, name='delete_ticket'),
     path('tickets/<str:pk>/', views.show, name='ticket'),
